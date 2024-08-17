9c9cc71cba80efa8d7999b111c597a77564cc98d
Tamannadk <tamannadk4@gmail.com>
2024-08-17 20:45:57 +0530

    fetch Api

---
+++ fetchApi/script.js
@@ -0,0 +1,11 @@
+// const image=document.querySelector("img")
+// const button=document.querySelector("button")
+
+// button.addEventListener("click",()=>{
+//     fetch('http://127.0.0.1:5500/fetchApi/index.html')
+//     .then((response)=>response.json())
+//     .then((json)=>{
+//         image.src=json.message
+//     })
+// })
+
